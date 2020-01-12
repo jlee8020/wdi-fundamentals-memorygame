9e66134977a6bd207f284afbc640b46d8b8282b1
Jan Lee <janlee@Jans-MacBook-Pro.local>
2020-01-11 19:16:02 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,17 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped" + cardOne);
+
+const cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards [0];
+cards.push("cardsInPlay");
+console.log("User flipped queen");
+
+var cardTwo = cards [2];
+cards.push("cardsInPlay");
+console.log("User flipped King");
+
+ if (cardsInPlay.length === 2) { 
+ 		if (cardsInPlay[0] === cardsInPlay[1]) 
+ 			alert("You found a match!"); 
+ 	} else {
+ 		alert("Sorry, try again");
+  }
